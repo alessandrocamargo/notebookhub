97c963db3e8061026a7ba8b9d719732ea32e52f5
alessandrocamargo <alessancamargo@gmail.com>
2026-09-18 14:38:51 -0300

    fix: implementa menu hamburguer mobile e corrige overflow horizontal da navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import {X, Menu} from "lucide-react"
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
-        <header className="bg-surface flex items-center justify-between px-4 py-4">
+        <header className="bg-surface flex flex-wrap items-center justify-between px-4 py-4">
             <h1 className="text-ink-primary"><Link to="/">Notebookhub</Link></h1>
-            <nav>
-                <ul className="flex items-center gap-6 text-ink-secondary">
+            {/*Botão Hamburguer - só aparece no mobile*/}
+            <button className="md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? <X /> : <Menu />}
+            </button>
+            {/* Links - visiveis sempre no desk, condicionalmente no mobile*/}
+            <nav className={`${isMenuOpen ? "block w-full" : "hidden"} md:block`}>
+                <ul className={`flex ${isMenuOpen ? "flex-col gap-1" : "gap-6"} text-ink-secondary`}>
                     <li><Link to="/" className="hover:text-ink-primary transition-colors">Home</Link></li>
                     <li><Link to="/notebooks" className="hover:text-ink-primary transition-colors">Notebooks</Link></li>
                     <li><Link to="/comparar" className="hover:text-ink-primary transition-colors">Comparar</Link></li>
